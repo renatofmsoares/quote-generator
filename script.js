f3d1f7890a94bac05680b9d1d4c5ff4fb39e9fe2
Renato Soares <renato.soares@tqi.com.br>
2023-03-21 20:16:30 -0300

    Fetching Data With JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("testing");
+let allQuotes = [];
+// Shownew quote
+function newQuote() {
+  // Pick a ramdom quote from apiQuotes array
+  const quote = allQuotes[Math.floor(Math.random() * allQuotes.length)];
+  console.log(quote);
+}
+
+async function getQuotesFromApi() {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    allQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Cattch error here
+  }
+}
+
+async function getQuotesFromLocalFile() {
+  allQuotes = localQuotes;
+}
+
+getQuotesFromLocalFile();
+newQuote();
