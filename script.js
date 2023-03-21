3b20b6ec55057ace38f4097b7d0a6d0bf470dad0
Renato Soares <renato.soares@tqi.com.br>
2023-03-21 20:51:31 -0300

    Manipulating DOM With JavaScript

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const quoteAuthor = document.getElementById("quote-author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 let allQuotes = [];
 // Shownew quote
 function newQuote() {
   // Pick a ramdom quote from apiQuotes array
   const quote = allQuotes[Math.floor(Math.random() * allQuotes.length)];
-  console.log(quote);
+  // console.log(quoteText.text);
+  quoteText.textContent = quote.text;
+  if (quote.autor) {
+    quoteAuthor.textContent = quote.author;
+  } else {
+    quoteAuthor.textContent = "Unknown";
+  }
+
+  // Check quote length to determine styling
+  console.log(quote.text.length);
+  if (quote.text.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
 }
 
 async function getQuotesFromApi() {
@@ -20,6 +40,15 @@ async function getQuotesFromApi() {
 async function getQuotesFromLocalFile() {
   allQuotes = localQuotes;
 }
+
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${quoteAuthor.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 getQuotesFromLocalFile();
 newQuote();
